fc505ec71dde9504cac2250f32b6e83a8fb51506
Philippe <pdelebarre@gmail.com>
2022-10-25 15:03:39 +0200

    exploring tsconfig

---
+++ ConfiguringTypeScript/dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+const norma = {
+    breed: "Silkie",
+    eggsPerWeek: 20,
+    name: "Norma",
+    age: 0.5,
+};
+const juniper = {
+    breed: "Juniper",
+    eggsPerWeek: 40,
+    name: "Norma",
+    age: 1,
+};
---
+++ ConfiguringTypeScript/src/index.ts
@@ -0,0 +1,20 @@
+interface Chicken {
+  breed: string;
+  eggsPerWeek: number;
+  name: string;
+  age: number;
+}
+
+const norma: Chicken = {
+  breed: "Silkie",
+  eggsPerWeek: 20,
+  name: "Norma",
+  age: 0.5,
+};
+
+const juniper: Chicken = {
+  breed: "Juniper",
+  eggsPerWeek: 40,
+  name: "Norma",
+  age: 1,
+};
